a3396a222226d4021f2888d7e3688c0307c9963b
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2026-05-13 23:09:20 -0300

    feat: responsive layout

--- src/home-page/Home.tsx
+++ src/home-page/Home.tsx
@@ -21,39 +21,61 @@ export default function Home() {
       default:
         return null;
     }
-  }
+  };
 
   return (
-      <div className={styles.background}>
-        <div className={styles.window}>
-          <div className={styles.windowHeader}>
-            <span>{activePage}</span>
-            <div className={styles.headerButtons}>
-              <button className={styles.windowButtons}>-</button>
-              <button className={styles.windowButtons}>□</button>
-              <button className={styles.windowButtons}>×</button>
-            </div>
+    <div className={styles.background}>
+      <div className={styles.window}>
+        <div className={styles.windowHeader}>
+          <span>{":-)"}</span>
+          <div className={styles.headerButtons}>
+            <button className={styles.windowButtons}>-</button>
+            <button className={styles.windowButtons}>□</button>
+            <button className={styles.windowButtons}>×</button>
           </div>
-  
-          <div className={styles.nav}>
-            {/* {["Home", "Studies", "Tools", "Resume/CV"].map((item) => (
+        </div>
+
+        <div className={styles.nav}>
+          {/* {["Home", "Studies", "Tools", "Resume/CV"].map((item) => (
               <button key={item} className={styles.navButton}>
                 {item}
               </button>
             ))} */}
-            <button onClick={() => setActivePage("Home")} className={styles.navButton}>🏡Home</button>
-            <button onClick={() => setActivePage("Studies")} className={styles.navButton}>📚Studies</button>
-            <button onClick={() => setActivePage("Career")} className={styles.navButton}>💼Career</button>
-            <button onClick={() => setActivePage("Travels")} className={styles.navButton}>🌍Travels</button>
-            <a download={true} href="/WalterFerreiraRamosEN.pdf" className={styles.navButton} >
-            📝Resume/CV
-            </a>
-          </div>
-  
-          <div className={styles.content}>
-            {renderContent()}
-          </div>
+          <button
+            onClick={() => setActivePage("Home")}
+            className={styles.navButton}
+          >
+            Home
+          </button>
+          <button
+            onClick={() => setActivePage("Studies")}
+            className={styles.navButton}
+          >
+            Studies
+          </button>
+          <button
+            onClick={() => setActivePage("Career")}
+            className={styles.navButton}
+          >
+            Career
+          </button>
+          <button
+            onClick={() => setActivePage("Travels")}
+            className={styles.navButton}
+          >
+            Travels
+          </button>
+          <a
+            download={true}
+            href="/WalterFerreiraRamosEN.pdf"
+            className={styles.navButton}
+          >
+            <span>Resume/CV</span>
+          </a>
         </div>
+
+        <div className={styles.content}>{renderContent()}</div>
       </div>
-  )
+    </div>
+  );
 }
--- src/home-page/components/HomePage/index.tsx
+++ src/home-page/components/HomePage/index.tsx
@@ -1,41 +1,86 @@
 import styles from "./HomePage.module.css";
-import sloth from '../../../assets/sloth.jpeg';
+import me from "../../../assets/me.jpg";
 
 export default function HomePage() {
   return (
     <>
       <div className={styles.profile}>
-      <img src={sloth} alt="Sloth" className={styles.profileImg} />
-      <div className={styles.info}>
-        <h2>
-          <strong>Walter Ferreira</strong>
-          <p className={styles.infoTitle}>Software Enginner</p>
-        </h2>
-        <p className={styles.infoMainText}>
-        With a degree in Computer Science and over four years of experience developing scalable web applications and APIs using <strong>React, Node.js, TypeScript, and Python.</strong> 
-        <br></br><br></br>I have built frontend applications with <strong>React and Next.js</strong>, designed and implemented RESTful APIs and backend services with <strong>Node.js and Python</strong>, and developed automation workflows and <strong>ETL pipelines</strong>.
-        <br></br><br></br> I am experienced with <strong>Docker, Kubernetes, CI/CD pipelines, and cloud platforms like Azure and AWS.</strong> Passionate about solving complex problems, I focus on delivering clean, maintainable, and scalable solutions while collaborating effectively in agile environments.
-        </p>
-        <p>
-          <strong>Available for hire:</strong>{" "}
-          <a href="mailto:devwalterjr@gmail.com">
-            devwalterjr@gmail.com
-          </a>
-        </p>
-        <p>
-        <strong>Socials:</strong> <a target="_blank" href="https://www.linkedin.com/in/walter-ferreira-161b871b9/">Linkedin</a> / <a target="_blank" href="https://github.com/ferreirawalter">Github</a>
-        </p>
+        <div className={styles.testes}>
+          <img src={me} alt="me" className={styles.profileImg} />
+          <div>
+            <h2>
+              <strong>Walter Ferreira</strong>
+            </h2>
+            <p className={styles.infoTitle}>Software Engineer</p>
+          </div>
+        </div>
+        <div className={styles.info}>
+          <p className={styles.infoMainText}>
+            With a degree in Computer Science and over +4 years of experience
+            developing scalable web applications and APIs using{" "}
+            <strong>React, Node.js, TypeScript, and Python.</strong>
+            <br></br>
+            <br></br>I have built frontend applications with{" "}
+            <strong>React and Next.js</strong>, designed and implemented RESTful
+            APIs and backend services with <strong>Node.js and Python</strong>,
+            and developed automation workflows and{" "}
+            <strong>ETL pipelines</strong>.<br></br>
+            <br></br> I am experienced with{" "}
+            <strong>
+              Docker, Kubernetes, CI/CD pipelines, and cloud platforms like
+              Azure and AWS.
+            </strong>{" "}
+            Passionate about solving complex problems, I focus on delivering
+            clean, maintainable, and scalable solutions while collaborating
+            effectively in agile environments.
+          </p>
+          <p>
+            <strong>Available for hire:</strong>{" "}
+            <a href="mailto:devwalterjr@gmail.com">devwalterjr@gmail.com</a>
+          </p>
+          <p>
+            <strong>Socials:</strong>{" "}
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/walter-ferreira-161b871b9/"
+            >
+              Linkedin
+            </a>{" "}
+            /{" "}
+            <a target="_blank" href="https://github.com/ferreirawalter">
+              Github
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
 
-    <h3>Projects</h3>
-    <div className={styles.work}>
-      <a target="_blank" href="https://github.com/FerreiraWalter/Teeny-Tiny-ORM">Teeny-Tiny-ORM</a>
-      <a target="_blank" href="https://github.com/FerreiraWalter/Teeny-Tiny-Compile">Teeny-Tiny-Compile</a>
-      <a target="_blank" href="https://github.com/FerreiraWalter/automatos">Automaton</a>
-      <a target="_blank" href="https://github.com/FerreiraWalter/lack">lack</a>
-      <a target="_blank" href="https://github.com/FerreiraWalter/SharinganOCR">OCR Sharingan</a>
-    </div>
+      <h3>Projects</h3>
+      <div className={styles.work}>
+        <a
+          target="_blank"
+          href="https://github.com/FerreiraWalter/Teeny-Tiny-ORM"
+        >
+          Teeny-Tiny-ORM
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/FerreiraWalter/Teeny-Tiny-Compile"
+        >
+          Teeny-Tiny-Compile
+        </a>
+        <a target="_blank" href="https://github.com/FerreiraWalter/automatos">
+          Automaton
+        </a>
+        <a target="_blank" href="https://github.com/FerreiraWalter/lack">
+          lack
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/FerreiraWalter/SharinganOCR"
+        >
+          OCR Sharingan
+        </a>
+      </div>
     </>
-  )
+  );
 }
